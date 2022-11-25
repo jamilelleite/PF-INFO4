ba90d2facba59c2ed98c1e15365cb181e57c0392
Jamile Lima Leite <jamilelleite@gmail.com>
2022-11-25 15:51:42 +0100

    Exercice 1.1.1

---
+++ TPs/Projet_PF-LT/projet_ocaml.ml
@@ -0,0 +1,46 @@
+
+(* Exercice 1.1.1 *)
+
+
+type bmvar =
+  | A
+  | B
+  | C
+  | D
+
+type bmexp =
+  | Exp of bmvar
+  | O (*false*)
+  | L (*true*)
+
+type wbm =
+  | MSkip 
+  | MAssign of bmvar * bmexp
+  | MSeq of wbm * wbm
+  | MIf of bmexp * wbm * wbm
+  | MWhile of bmexp * wbm
+
+
+
+let if1_1: wbm = MAssign(C, O)
+
+let if1_2: wbm = MAssign(A, Exp B)
+
+let if2_1: wbm = MAssign(B, O)
+
+let if2_2: wbm = MAssign(C, Exp A)
+
+let if1: wbm = MSeq(if1_1, if1_2)
+
+let if2: wbm = MSeq(if2_1, if2_2)
+
+let if0_0: wbm = MIf(Exp C, if1, if2)
+
+let wh: wbm = MWhile(Exp A, if0_0)
+
+(* Exercice 1.1.2 *)
+
+
+
+
+
