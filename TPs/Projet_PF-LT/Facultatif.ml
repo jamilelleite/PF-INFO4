0a2c871ec442e2b27e3b374c60d657741a1f8d9e
Jamile Lima Leite <jamilelleite@gmail.com>
2022-12-09 16:30:41 +0100

    Functions names changed

--- TPs/Projet_PF-LT/Facultatif.ml
+++ TPs/Projet_PF-LT/Facultatif.ml
@@ -30,28 +30,18 @@ let li2 = li1()
 let _ = get 10 li1
 
 (* Le type des aspirateurs (fonctions qui aspirent le préfixe d'une liste) *)
-type 'term analist = 'term list -> 'term list
-
-type 'term panalist = 'term lazylist -> 'term lazylist
+type 'term analist = 'term lazylist -> 'term lazylist
 
 exception Echec
 
 (* terminal constant *)
-let terminal (c : 't) : 't analist = function
-  | x :: l when x = c -> l
-  | _ -> raise Echec
-
-let pterminal (c: 't): 't panalist = function
+let terminal (c: 't): 't analist = function
   | unit -> match unit() with
             | Cons(x,l) -> if (x=c) then l else raise Echec
             | Nil -> raise Echec
                 
 (* terminal conditionnel *)
-let terminal_cond (p : 'term -> bool) : 'term analist = function
-  | x :: l when p x -> l
-  | _ -> raise Echec
-
-let pterminal_cond (p: 'term -> bool) : 'term panalist = function
+let terminal_cond (p: 'term -> bool) : 'term analist = function
   | unit -> match unit() with
             | Cons(x,l) -> if (p x) then l else raise Echec
             | Nil -> raise Echec
@@ -59,19 +49,17 @@ let pterminal_cond (p: 'term -> bool) : 'term panalist = function
 (* non-terminal vide *)
 let epsilon : 'term analist = fun l -> l
 
-let pepsilon : 'term panalist = fun l -> l
-
-let p_5: int panalist = pterminal 5
+let p_5: int analist = terminal 5
 let new_lf1 = p_5 lf1
 let _ = get 20 new_lf1
 
-let p_6: int panalist = pterminal 6
+let p_6: int analist = terminal 6
 
-let p_20: int panalist = pterminal 20
+let p_20: int analist = terminal 20
 let new_li1 = p_20 li1
 let _ = get 5 new_li1
 
-let ts = pepsilon lf1
+let ts = epsilon lf1
 let _ = get 10 ts
 
 (* ------------------------------------------------------------ *)
@@ -82,25 +70,19 @@ let _ = get 10 ts
 let (-->) (a1 : 'term analist) (a2 : 'term analist) : 'term analist =
   fun l -> let l = a1 l in a2 l
 
-let (-->>) (a1: 'term panalist) (a2: 'term panalist) : 'term panalist =
-  fun l -> let l = a1 l in a2 l
-
 (* Choix entre a1 ou a2 *)
 let (-|) (a1 : 'term analist) (a2 : 'term analist) : 'term analist =
   fun l -> try a1 l with Echec -> a2 l
 
-let (-||) (a1: 'term panalist) (a2: 'term panalist) : 'term panalist =
-  fun l -> try a1 l with Echec -> a2 l
+let p_5_6: int analist =
+  p_5 --> p_6
 
-let p_5_6: int panalist =
-  p_5 -->> p_6
-
-let p_20_21: int panalist =
-  (pterminal 20) -->> (pterminal 21)
+let p_20_21: int analist =
+  (terminal 20) --> (terminal 21)
 
 let _ = get 10 (p_20_21 li1)
 
-let p_a: int panalist = p_5_6 -|| p_20_21
+let p_a: int analist = p_5_6 -| p_20_21
 let _ = get 10 (p_a li1)
 let _ = get 10 (p_a lf1)
 
@@ -115,43 +97,27 @@ let rec star (a : 'term analist) : 'term analist = fun l -> l |>
 (* ------------------------------------------------------------ *)
 
 (* Le type des aspirateurs qui, en plus, rendent un résultat *)
-type ('res, 'term) ranalist = 'term list -> 'res * 'term list
-
-type ('res, 'term) pranalist = 'term lazylist -> 'res * 'term lazylist
+type ('res, 'term) ranalist = 'term lazylist -> 'res * 'term lazylist
 
 (* Un epsilon informatif *)
 let epsilon_res (info : 'res) : ('res, 'term) ranalist =
   fun l -> (info, l)
 
-let pepsilon_res (info: 'res) : ('res, 'term) pranalist =
-  fun l -> (info, l)
-
 (* Terminal conditionnel avec résultat *)
 (* [f] ne retourne pas un booléen mais un résultat optionnel *)
 let terminal_res (f : 'term -> 'res option) : ('res, 'term) ranalist =
-  fun l -> match l with
-  | x :: l -> (match f x with Some y -> y, l | None -> raise Echec)
-  | _ -> raise Echec
-
-let pterminal_res (f: 'term -> 'res option) : ('res, 'term) pranalist =
   fun l -> match l() with
-           | Cons(x,l) -> (match f x with Some y -> y, l | None -> raise Echec)
-           | Nil -> raise Echec
+  | Cons(x, l) -> (match f x with Some y -> y, l | None -> raise Echec)
+  | _ -> raise Echec
 
 (* a1 sans résultat suivi de a2 donnant un résultat *)
 let ( -+>) (a1 : 'term analist) (a2 : ('res, 'term) ranalist) :
       ('res, 'term) ranalist =
   fun l -> let l = a1 l in a2 l
 
-let (-+>>) (a1: 'term panalist) (a2: ('res, 'term) pranalist) : ('res, 'term) pranalist =
-  fun l -> let l = a1 l in a2 l
-
 (* a1 rendant un résultat suivi de a2 rendant un résultat *)
 let (++>) (a1 : ('resa, 'term) ranalist) (a2 : 'resa -> ('resb, 'term) ranalist) :
       ('resb, 'term) ranalist =
-  fun l -> let (x, l) = a1 l in a2 x l
-
-let (++>>) (a1: ('resa, 'term) pranalist) (a2: 'resa -> ('resb, 'term) pranalist) : ('resb, 'term) pranalist =
   fun l -> let (x, l) = a1 l in a2 x l
 
 (* a1 rendant un résultat suivi de a2 sans résultat est peu utile *)
@@ -161,6 +127,12 @@ let (+|) (a1 : ('res, 'term) ranalist) (a2 : ('res, 'term) ranalist) :
       ('res, 'term) ranalist =
   fun l -> try a1 l with Echec -> a2 l
 
-let (+||) (a1: ('res, 'term) pranalist) (a2: ('res, 'term) pranalist) : ('res, 'term) pranalist =
-  fun l -> try a1 l with Echec -> a2 l
+type aexp =
+  | Const of int
+  | Amu of aexp * aexp
+
+let pr_5: (aexp, int) ranalist = terminal 5 -+> epsilon_res (Const 5)
+let pr_6: (aexp, int) ranalist = terminal 6 -+> epsilon_res (Const 6)
+let pr_20: (aexp, int) ranalist = terminal 20 -+> epsilon_res (Const 20)
+let pr_21: (aexp, int) ranalist = terminal 21 -+> epsilon_res (Const 21)
 
